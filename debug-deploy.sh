d33ee2a2dfe81ee0df6e8bcec1655399c6a6bb46
Valerio Casalino <casalinovalerio.cv@gmail.com>
2021-01-15 22:33:58 +0100

    Dockerized :)

--- debug-deploy.sh
+++
@@ -1,9 +0,0 @@
-export SEUS_DBHOST="localhost"
-export SEUS_DBPORT="8081"
-export SEUS_DBUSER="test"
-export SEUS_DBPASS="password"
-export SEUS_DBNAME="seus"
-export SEUS_SSLCRT="$PWD/debug/localhost.crt"
-export SEUS_SSLKEY="$PWD/debug/localhost.key"
-
-go run ./main.go
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -10,29 +12,41 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var (
-	// DBHost is the address of the Database
-	DBHost = os.Getenv("SEUS_DBHOST")
-	// DBPort is the port in which the DB is reachable
-	DBPort = os.Getenv("SEUS_DBPORT")
-	// DBUser is the username of mongodb
-	DBUser = os.Getenv("SEUS_DBUSER")
-	// DBPass is the password for the user
-	DBPass = os.Getenv("SEUS_DBPASS")
-	// DBName is the name of the database
-	DBName = os.Getenv("SEUS_DBNAME")
-	// SSLCrt is the SSL certificate location
-	SSLCrt = os.Getenv("SEUS_SSLCRT")
-	// SSLKey is the SSL key location
-	SSLKey = os.Getenv("SEUS_SSLKEY")
-	// DBClient is the object representing the connection
-	DBClient interface{}
-)
+// ConfigFile is the file that loads the user configurations
+const ConfigFile = "./config.json"
+
+// ConfigHost is a wrapper for configs about the host
+type ConfigHost struct {
+	Domain    string `json:"domain"`
+	HTTPPort  string `json:"httpport"`
+	HTTPSport string `json:"httpsport"`
+}
+
+// ConfigDB is a wrapper for configs about the db
+type ConfigDB struct {
+	Domain string `json:"domain"`
+	Port   string `json:"port"`
+	User   string `json:"user"`
+	Passwd string `json:"passwd"`
+	Name   string `json:"name"`
+}
+
+// ConfigSSL is a wrapper for configs about the certs
+type ConfigSSL struct {
+	Cert string `json:"cert"`
+	Key  string `json:"key"`
+}
+
+// Config wraps the configurations in
+type Config struct {
+	Host ConfigHost `json:"host"`
+	DB   ConfigDB   `json:"db"`
+	SSL  ConfigSSL  `json:"ssl"`
+}
 
 // Redirect redirects the requests from http to https
-func Redirect(w http.ResponseWriter, req *http.Request) {
-	// target := "https://" + req.Host + req.URL.Path
-	target := "https://localhost:8081"
+func Redirect(w http.ResponseWriter, req *http.Request, c Config) {
+	target := "https://" + c.Host.Domain + ":" + c.Host.HTTPSport
 	if len(req.URL.RawQuery) > 0 {
 		target += "?" + req.URL.RawQuery
 	}
@@ -41,7 +55,8 @@ func Redirect(w http.ResponseWriter, req *http.Request) {
 }
 
 // ConnectDB is a function that connects the database and returns the client
-func ConnectDB() (interface{}, error) {
+func ConnectDB(c Config) (interface{}, error) {
+	DBUser, DBPass, DBHost, DBPort, DBName := c.DB.User, c.DB.Passwd, c.DB.Domain, c.DB.Port, c.DB.Name
 	DBConnectionURI := fmt.Sprintf("mongodb://%v:%v@%v:%v/%v", DBUser, DBPass, DBHost, DBPort, DBName)
 	client, err := mongo.NewClient(options.Client().ApplyURI(DBConnectionURI))
 	if err != nil {
@@ -51,7 +66,7 @@ func ConnectDB() (interface{}, error) {
 }
 
 // URLRedirectWithCode gets a code and redirects to the shortened URL
-func URLRedirectWithCode(w http.ResponseWriter, r *http.Request) {
+func URLRedirectWithCode(w http.ResponseWriter, r *http.Request, c Config) {
 	//code := r.URL.Path[1:]
 	target := "https://www.google.com"
 
@@ -59,13 +74,27 @@ func URLRedirectWithCode(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var configs Config
+
+	reader, err := ioutil.ReadFile(ConfigFile)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	if err := json.Unmarshal([]byte(reader), &configs); err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 
 	// Redirect from port 80 to port 443
-	go http.ListenAndServe(":8080", http.HandlerFunc(Redirect))
+	go http.ListenAndServe(":8080", http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		Redirect(rw, r, configs)
+	}))
 
 	// Handle base path (even with code)
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		URLRedirectWithCode(rw, r)
+		URLRedirectWithCode(rw, r, configs)
 	})
 
 	// Api handling
@@ -82,5 +111,5 @@ func main() {
 	})
 
 	fmt.Println("Server started at port 8081")
-	log.Fatal(http.ListenAndServeTLS(":8081", SSLCrt, SSLKey, nil))
+	log.Fatal(http.ListenAndServeTLS(":8081", configs.SSL.Cert, configs.SSL.Key, nil))
 }
