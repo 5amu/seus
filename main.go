e9fe7d02b505a229121b713fa8fe741984994592
Valerio Casalino <casalinovalerio.cv@gmail.com>
2021-01-15 01:15:56 +0100

    Redirect to https

--- main.go
+++ main.go
@@ -29,6 +29,17 @@ var (
 	DBClient interface{}
 )
 
+// Redirect redirects the requests from http to https
+func Redirect(w http.ResponseWriter, req *http.Request) {
+	// target := "https://" + req.Host + req.URL.Path
+	target := "https://localhost:8081"
+	if len(req.URL.RawQuery) > 0 {
+		target += "?" + req.URL.RawQuery
+	}
+	log.Printf("redirect to: %s", target)
+	http.Redirect(w, req, target, http.StatusTemporaryRedirect)
+}
+
 // ConnectDB is a function that connects the database and returns the client
 func ConnectDB() (interface{}, error) {
 	DBConnectionURI := fmt.Sprintf("mongodb://%v:%v@%v:%v/%v", DBUser, DBPass, DBHost, DBPort, DBName)
@@ -42,12 +53,34 @@ func ConnectDB() (interface{}, error) {
 // URLRedirectWithCode gets a code and redirects to the shortened URL
 func URLRedirectWithCode(w http.ResponseWriter, r *http.Request) {
 	//code := r.URL.Path[1:]
-	fmt.Print("LOL")
+	target := "https://www.google.com"
+
+	http.Redirect(w, r, target, http.StatusTemporaryRedirect)
 }
 
 func main() {
 
-	http.HandleFunc("/", URLRedirectWithCode)
-	fmt.Println("Server started at port 8080")
-	log.Fatal(http.ListenAndServeTLS(":8080", SSLCrt, SSLKey, nil))
+	// Redirect from port 80 to port 443
+	go http.ListenAndServe(":8080", http.HandlerFunc(Redirect))
+
+	// Handle base path (even with code)
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		URLRedirectWithCode(rw, r)
+	})
+
+	// Api handling
+	http.HandleFunc("/api", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(rw, "Hit api")
+	})
+
+	http.HandleFunc("/api/create", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(rw, "Hit api/create")
+	})
+
+	http.HandleFunc("/api/search", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(rw, "Hit api/search")
+	})
+
+	fmt.Println("Server started at port 8081")
+	log.Fatal(http.ListenAndServeTLS(":8081", SSLCrt, SSLKey, nil))
 }
