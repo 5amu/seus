e44450ae85fc3c8915e7100fed99952ec42e1c39
Valerio Casalino <casalinovalerio.cv@gmail.com>
2021-01-16 23:11:33 +0100

    Better handling of codes

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -57,6 +58,15 @@ type Seus struct {
 	// Counter int    `json:"counter,omitempty"`
 }
 
+// SeusResponse is a wrapper for json response
+type SeusResponse struct {
+	Status  int    `json:"status,omitempty"`
+	Message string `json:"message,omitempty"`
+	URL     string `json:"url,omitempty"`
+	Code    string `json:"code,omitempty"`
+	Encoded string `json:"encoded,omitempty"`
+}
+
 // Redirect redirects the requests from http to https
 func Redirect(w http.ResponseWriter, req *http.Request, c Config) {
 	target := "https://" + c.Host.Domain + ":" + c.Host.HTTPSport
@@ -67,8 +77,7 @@ func Redirect(w http.ResponseWriter, req *http.Request, c Config) {
 	http.Redirect(w, req, target, http.StatusTemporaryRedirect)
 }
 
-// ConnectDB is a function that connects the database and returns the client
-func ConnectDB(c Config) (*mongo.Client, error) {
+func connectDB(c Config) (*mongo.Client, error) {
 	DBUser, DBPass, DBHost, DBPort := c.DB.User, c.DB.Passwd, c.DB.Domain, c.DB.Port
 	DBConnectionURI := fmt.Sprintf("mongodb://%v:%v@%v:%v", DBUser, DBPass, DBHost, DBPort)
 	client, err := mongo.NewClient(options.Client().ApplyURI(DBConnectionURI))
@@ -78,55 +87,66 @@ func ConnectDB(c Config) (*mongo.Client, error) {
 	return client, nil
 }
 
+func getResult(c Config, filter primitive.D) (Seus, error) {
+	var res Seus
+	client, err := connectDB(c)
+	if err != nil {
+		return res, err
+	}
+	defer client.Disconnect(context.Background())
+	collection := client.Database(c.DB.Name).Collection(c.DB.Collection)
+	err = collection.FindOne(context.Background(), filter).Decode(&res)
+	if err != nil {
+		return res, err
+	}
+	return res, nil
+}
+
 // URLRedirectWithCode gets a code and redirects to the shortened URL
 func URLRedirectWithCode(w http.ResponseWriter, r *http.Request, c Config) error {
-	var res Seus
-
 	// Get the code
 	code := r.URL.Path[1:]
-
-	// Connect to the DB
-	dbclient, err := ConnectDB(c)
-	if err != nil {
-		return err
-	}
-	defer dbclient.Disconnect(context.Background())
-
 	// Define filters
 	filter := bson.D{bson.E{Key: "code", Value: code}}
-	// Access the collection
-	collection := dbclient.Database(c.DB.Name).Collection(c.DB.Collection)
-	// Run the query
-	err = collection.FindOne(context.Background(), filter).Decode(&res)
-	if err != mongo.ErrNoDocuments {
-		if err != nil {
-			return err
+	// Get result by filter
+	res, err := getResult(c, filter)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			out := new(SeusResponse)
+			out.Status = 404
+			out.Code = code
+			out.Message = "Code not found"
+			mar, _ := json.Marshal(out)
+			fmt.Fprintf(w, string(mar))
 		}
+		return err
 	}
-
-	target := res.URL
-
-	http.Redirect(w, r, target, http.StatusTemporaryRedirect)
+	log.Printf("Redirecting IP %v with code %v to URL %v", r.RemoteAddr, code, res.URL)
+	// Redirect to correct URL
+	http.Redirect(w, r, res.URL, http.StatusTemporaryRedirect)
 	return nil
 }
 
 func main() {
 	var configs Config
-
+	// Open LOG file in append mode (or create mode)
 	f, err := os.OpenFile(LogFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 	defer f.Close()
+	// Set log target to the file just opened
 	log.SetOutput(f)
 
+	// Open config file
 	reader, err := ioutil.ReadFile(ConfigFile)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 
+	// Parse config file
 	if err := json.Unmarshal([]byte(reader), &configs); err != nil {
 		log.Fatal(err)
 		os.Exit(1)
@@ -157,6 +177,7 @@ func main() {
 		fmt.Fprintf(rw, "Hit api/search")
 	})
 
+	// Log the start of the server + start listen on https
 	log.Println("Server started at port 8080 and 8443")
 	log.Fatal(http.ListenAndServeTLS(":"+configs.Host.HTTPSport, configs.SSL.Cert, configs.SSL.Key, nil))
 }
